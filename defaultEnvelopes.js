e4f552a180d5d0ae4f1dbe0f66ddb4957fafbcd2
Masutuda <rockoppa@outlook.com>
2022-07-12 16:54:54 -0700

    Added API functions and default envelopes

---
+++ defaultEnvelopes.js
@@ -0,0 +1,34 @@
+const defaultEnvelopes = [
+    {   
+        id: 1,
+        envelopeName: 'Home Mortgage',
+        maxBudgetByAmount: 2000,
+        amountLeft: 0
+    },
+    {
+        id: 2,
+        envelopeName: 'Auto Loan & Insurance',
+        maxBudgetByAmount: 2000,
+        amountLeft: 0
+    },
+    {
+        id: 3,
+        envelopeName: 'Savings',
+        maxBudgetByAmount: 2000,
+        amountLeft: 0
+    },
+    {
+        id: 4,
+        envelopeName: 'Groceries',
+        maxBudgetByAmount: 2000,
+        amountLeft: 0
+    },
+    {
+        id: 5,
+        envelopeName: 'Utilities',
+        maxBudgetByAmount: 2000,
+        amountLeft: 0
+    }
+];
+
+module.exports = {defaultEnvelopes};
--- index.js
+++ index.js
@@ -1,7 +1,66 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
+const {defaultEnvelopes} = require('./defaultEnvelopes.js');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+let totalBudget;
+let budgetEnvelopes = [];
+budgetEnvelopes = defaultEnvelopes;
 
-app.listen(port, () => console.log(`LISTENING ON PORT: ${port}!`));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use('/envelopes/:envelopeId', (req, res, next) => {
+    const envelopeId = Number(req.params.envelopeId);
+    const envelopeIndex = budgetEnvelopes.findIndex(envelope => envelope.id === envelopeId);
+    if (envelopeIndex === -1) {
+      return res.status(404).send('Envelope not found');
+    }
+    req.envelopeIndex = envelopeIndex;
+    next();
+  });
+
+
+const validateEnvelope = (req, res, next) => {
+    const newEnvelope = req.body;
+    newEnvelope.maxBudgetByAmount = Number(newEnvelope.maxBudgetByAmount);
+    newEnvelope.amountLeft = Number(newEnvelope.amountLeft);
+    next();
+}
+
+
+// Return all Envelopes
+app.get('/envelopes', (req, res) => {
+    res.send(budgetEnvelopes);
+});
+
+// Return single Envelope
+app.get('/envelopes/:id', (req, res) => {
+    res.send(budgetEnvelopes[req.envelopeIndex]);
+});
+
+// Add Envelope
+app.post('/envelopes', validateEnvelope, (req, res) => {
+    const newEnvelope = req.body;
+    newEnvelope.id = budgetEnvelopes.length + 1;
+    budgetEnvelopes.push(newEnvelope);
+    res.status(201).send(newEnvelope);
+});
+
+// Update Envelope
+app.put('/envelopes/:id', validateEnvelope, (req, res) => {
+    budgetEnvelopes[req.envelopeIndex] = req.body;
+    res.send(req.body);
+});
+
+// Delete Envelope
+app.delete('/envelopes/:id', (req, res) => {
+    const deletedEnvelope = budgetEnvelopes[req.envelopeIndex];
+    budgetEnvelopes.splice(req.envelopeIndex, 1);
+    res.send(deletedEnvelope);
+    res.status(204);
+});
+
+
+app.listen(port, () => console.log(`Listening on port: ${port}...`));
